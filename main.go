fe02fb99ba8ca4affd93b096dc1b5efe3c8d5992
Jauws <fleuryjoel.rakotomalala@ynov.fr>
2024-05-29 14:32:01 +0200

    Mise en place de Chi en microservice

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"forum/application"
+)
+
+func main() {
+	app := application.New()
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start app", err)
+	}
+}
