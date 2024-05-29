a803ec7fb80c78abbbb24a5ebafe0dcb18168f30
Jauws <fleuryjoel.rakotomalala@ynov.fr>
2024-05-29 15:31:34 +0200

    Handler route racine letrésboncoin

--- application/route.go
+++ application/route.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"forum/handler"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,6 +14,17 @@ func LoadRoutes() *chi.Mux {
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+	router.Route("/Letrésboncoin", loadOrderRoutes)
 	return router
 
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{ID}", orderHandler.GetByID)
+	router.Put("/{ID}", orderHandler.UpdateByID)
+	router.Delete("/{ID}", orderHandler.DeleteByID)
 }
